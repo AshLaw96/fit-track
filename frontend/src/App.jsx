c705f725355aa36f668e2ad5fbf2513b525a17e8
AshLaw96 <ash.l.96@outlook.com>
2025-05-13 22:12:21 +0100

    Add global btn style & change import order of auth css & bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -13,33 +13,37 @@ import PasswordResetConfirmForm from "./components/PasswordResetConfirmForm";
 
 const App = () => {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/reset-password" element={<PasswordResetForm />} />
-            <Route path="/reset-password-confirm/:uid/:token" element={<PasswordResetConfirmForm />} />
-            <Route path="*" element={<div className="text-center p-4">404 - Page Not Found</div>} />
-          </Routes>
-        </main>
-        <Footer />
-        <AutoScrollUp />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </div>
-    </Router>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/reset-password" element={<PasswordResetForm />} />
+          <Route
+            path="/reset-password-confirm/:uid/:token"
+            element={<PasswordResetConfirmForm />}
+          />
+          <Route
+            path="*"
+            element={<div className="text-center p-4">404 - Page Not Found</div>}
+          />
+        </Routes>
+      </main>
+      <Footer />
+      <AutoScrollUp />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
   );
 };
 
