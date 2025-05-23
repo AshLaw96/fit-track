e0fd14ecebf59c0d52cf5df20e9908c73d37d6c0
AshLaw96 <ash.l.96@outlook.com>
2025-05-23 22:00:01 +0100

    Create settings toggle file with custom style

---
+++ frontend/src/components/SettingToggle.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SettingItem.css";
+
+const SettingToggle = ({ label, Icon, enabled, onToggle }) => {
+  return (
+    <div className="setting-item toggle">
+      <div className="setting-label">
+        {Icon && <Icon className="setting-icon" />}
+        <span>{label}</span>
+      </div>
+      <label className="switch">
+        <input type="checkbox" checked={enabled} onChange={onToggle} />
+        <span className="slider round"></span>
+      </label>
+    </div>
+  );
+};
+
+export default SettingToggle;
