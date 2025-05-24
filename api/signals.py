0bfabd0f16c54c4276d91015057263b0e2efc76c
AshLaw96 <ash.l.96@outlook.com>
2025-05-24 20:33:19 +0100

    Create daily motivation signal & delete app css & logo as unused

--- api/signals.py
+++ api/signals.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from django.utils.timezone import now
 from django.db.models.signals import post_save
-from .models import UserActivity, CustomUser
+from .models import UserActivity, CustomUser, Notification
 from .utils.activity import calculate_user_streak
+from .utils.notifications import send_notification
 
 
 @receiver(post_save, sender=CustomUser)
@@ -15,3 +17,23 @@ def create_user_activity(sender, instance, created, **kwargs):
 def update_user_streak(sender, request, user, **kwargs):
     activity, created = UserActivity.objects.get_or_create(user=user)
     calculate_user_streak(activity)
+
+
+@receiver(user_logged_in)
+def send_daily_motivation(sender, user, request, **kwargs):
+    today = now().date()
+
+    # Avoid duplicate reminders on same day
+    if Notification.objects.filter(
+        user=user,
+        type='reminder',
+        timestamp__date=today
+    ).exists():
+        return
+
+    send_notification(
+        user=user,
+        title="New Day, New Energy 💪",
+        message="Make today count. You're stronger than you think.",
+        type="reminder"
+    )
